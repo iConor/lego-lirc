b19ae85a35d4d60a913e6e341eab58c7a85567cd
Conor Cary <cary.42@osu.edu>
2016-05-09 22:38:25 -0400

    Adding built distribution capabilities.

--- setup.py
+++ setup.py
@@ -1,29 +1,26 @@
+'''LEGO LIRC :: setup.py'''
+
 from setuptools import setup, find_packages
 
 with open('README.rst') as f:
-    readme = f.read()
-
-with open('LICENSE') as f:
-    license = f.read()
+    README = f.read()
 
 setup(
-    name = 'lego-lirc',
-    version = '0.2.1',
-    description = 'Control LEGO Power Functions with a Raspberry Pi and an LED, using LIRC (Linux Infrared Remote Control).',
-    long_description = readme,
-    author = 'Conor Cary',
-    author_email = 'cary.42@osu.edu',
-    url = 'http://github.com/iConor/lego-lirc',
-    license = license,
-    packages = find_packages(exclude=('config-files', 'docs', 'tests')),
-    classifiers = (
+    name='lego-lirc',
+    version='0.2.1',
+    description='Control LEGO Power Functions with a Raspberry Pi and an IR LED, using LIRC.',
+    long_description=README,
+    author='Conor Cary',
+    author_email='cary.42@osu.edu',
+    url='http://github.com/iConor/lego-lirc',
+    license='MIT',
+    packages=find_packages(exclude=('config-files', 'docs', 'tests')),
+    classifiers=(
         'Development Status :: 4 - Beta',
         # 'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
+        # 'Intended Audience :: Developers',
+        # 'Intended Audience :: Education',
         'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Operating System :: MacOS :: MacOS X',
@@ -32,14 +29,14 @@ setup(
         # 'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         # 'Programming Language :: Python :: 3',
-        'Topic :: Communications',
-        'Topic :: Education',
-        'Topic :: Games/Entertainment',
-        'Topic :: Home Automation',
-        'Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator',
-        'Topic :: Software Development :: Embedded Systems',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: System :: Hardware :: Hardware Drivers',
-        'Topic :: Utilities'
-    )
+        # 'Topic :: Communications',
+        # 'Topic :: Education',
+        # 'Topic :: Games/Entertainment',
+        # 'Topic :: Home Automation',
+        # 'Topic :: Software Development :: Embedded Systems',
+        # 'Topic :: Software Development :: Libraries :: Python Modules',
+        # 'Topic :: System :: Hardware :: Hardware Drivers'
+    ),
+    keywords=['lego', 'lirc', 'power', 'functions', 'powerfunctions', 'linux',
+              'inrared', 'remote', 'control', 'raspberry', 'pi']
 )
